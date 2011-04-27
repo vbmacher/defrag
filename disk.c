21ed77b541f187f0ee08ab33c5426745128876a8
Peter Jakubčo <pjakubco@gmail.com>
2011-04-27 07:49:09 +0200

    Added license text into source files

--- disk.c
+++ disk.c
@@ -10,7 +10,23 @@
  * descriptor called disk_descriptor and on given location they perform the operations.
  *
  */
-/* The module I've started to write at day: 1.11.2006 */
+
+/* The module I've started to write at day: 1.11.2006 
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 #include <stdio.h>
 #include <unistd.h>
